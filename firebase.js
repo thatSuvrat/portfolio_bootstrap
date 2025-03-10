6d8081b9df7239e0c8df1278626bf4001f774366
Suvrat Bhatta <71361002+thatSuvrat@users.noreply.github.com>
2025-03-10 17:45:20 +0545

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmE98vGwyffenbgan3TywsZnxY64p4LbY",
-  authDomain: "suvrat-bhatta.firebaseapp.com",
-  projectId: "suvrat-bhatta",
-  storageBucket: "suvrat-bhatta.appspot.com",
-  messagingSenderId: "54162575835",
-  appId: "1:54162575835:web:0ddeaa2546ca60fbf2d7fe",
-  measurementId: "G-X4EB53J662"
+  // REDACTED
 };
 
 // Initialize Firebase
